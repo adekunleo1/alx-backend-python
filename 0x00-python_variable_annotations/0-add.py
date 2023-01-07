314949144ae2b081e480cc72d4909b08d77e8327
adekunleo1 <adekunle.ogunleye1@gmail.com>
2023-01-07 10:31:11 +0100

    0-add.py task

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
 '''
-Write a type-annotated function 'add' that takes
-a float 'a' and a float 'b' 
-as arguments and returns their sum as a float.
+A module to add a typed annotated function
 '''
-
 
 def add(a: float, b: float) -> float:
     '''Adds two float integers'''
